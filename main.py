5647290e34088440fa8087c2e9d0d9cb51daad30
Robert-Phan <fannk987@gmail.com>
2025-07-23 11:51:59 +0700

    Got compactment to work

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import csv
 from typing import cast
 import xml.etree.ElementTree as ET
-from svgpathtools import parse_path
+from svgpathtools import parse_path, Path
+import svgpathtools.path as pathtools 
 import math
-import copy
+from move import Rect, movement_to_touch, move_with_obstacles
 from ev_dict import create_ev_dict
-from centroid_and_area import compute_svg_areas, path_centroid, centroid_of_path_element
+from centroid_and_area import compute_svg_areas, centroid_of_path_element
 import re
+from pprint import pprint
 
 INPUT = "1968.svg"
 NTH_APP = 23
@@ -73,6 +75,16 @@ def scale_svg(input_svg: str,
             t = f"translate({cx},{cy}) scale({s}) translate({-cx},{-cy})"
             existing = path_el.get('transform', '')
             path_el.set('transform', f"{existing} {t}".strip())
+    
+    id_to_rect: dict[str, Rect] = {}
+    id_to_centroid: dict[str, tuple[float, float]] = {}
+
+    target_x: float = 0
+    target_y: float = 0
+
+    outline_el = root.find(".//svg:*[@id='outlines']", namespaces=ns)
+    if outline_el is None:
+        return
 
     # 2. Standalone <path> scaling: if a <path> itself has an ID in id_to_scale
     for path_el in root.findall('.//svg:path', ns):
@@ -85,30 +97,73 @@ def scale_svg(input_svg: str,
         if not d:
             continue
 
-        path_el_copy = copy.deepcopy(path_el)
-        path_el_copy.set('stroke-width', '0.5')
-        path_el_copy.set('stroke', '#4772b8')
-        path_el_copy.set('fill', '#edf1f8')
-        path_el_copy.set('id', path_id + 'c')
-
-        outline_el = root.find(".//svg:*[@id='outlines']", namespaces=ns)
-        if outline_el is not None:
-            outline_el.insert(0, path_el_copy)
+        # path_el_copy = copy.deepcopy(path_el)
+        # path_el_copy.set('stroke-width', '0.5')
+        # path_el_copy.set('stroke', '#4772b8')
+        # path_el_copy.set('fill', '#edf1f8')
+        # path_el_copy.set('id', path_id + 'c')
 
         path = parse_path(d)
         centroid = centroid_of_path_element(path)
         if not centroid:
             continue
-
         cx, cy = centroid.real, centroid.imag
 
-        t = f"translate({cx},{cy}) scale({s}) translate({-cx},{-cy})"
-        existing = path_el.get('transform', '')
-        path_el.set('transform', f"{existing} {t}".strip())
+        cc = complex(cx, cy)
+        new_path = cast(Path, path.scaled(s, origin=cc))
+        d = new_path.d()
+        path_el.set('d', d)
+
+        rect = Rect(*new_path.bbox())
+        id_to_rect[path_id] = rect
+        id_to_centroid[path_id] = cx, cy
+        if path_id == 'RI':
+            target_x, target_y = cx, cy
+
+        # t = f"translate({cx},{cy}) scale({s}) translate({-cx},{-cy})"
+        # existing = path_el.get('transform', '')
+        # path_el.set('transform', f"{existing} {t}".strip())
+
         path_el.set('fill', '#4772b8')
         path_el.set('stroke-width', '0.5')
         path_el.set('stroke', '#325081')
         path_el.set('vector-effect', 'non-scaling-stroke')
+
+    target = id_to_rect['RI']
+    id_to_distance = {k: math.hypot(target_x - x, target_y - y) for k, (x, y) in id_to_centroid.items()}
+    nearest_to_farthest = sorted(id_to_distance, key=lambda k: id_to_distance[k])
+
+    for i, path_id in enumerate(nearest_to_farthest):
+        print(path_id)
+
+        xpath = f".//svg:path[@id='{path_id}']"
+        path_el = root.find(xpath, ns)
+
+        if path_el is None:
+            continue
+        
+        moving = id_to_rect[path_id]
+        obstacles = [id_to_rect[id] for j, id in enumerate(nearest_to_farthest) if j < i]
+        # print(obstacles[-1] if len(obstacles) > 0 else None)
+
+        # dx, dy = movement_to_touch(moving, target)
+        dx, dy = move_with_obstacles(moving, target, obstacles)
+        
+        d = path_el.get('d')
+        if not d:
+            continue
+        path = parse_path(d)
+        
+        new_path = cast(Path, path.translated(complex(dx, dy)))
+        rect = Rect(*new_path.bbox())
+        id_to_rect[path_id] = rect
+        # print(rect)
+
+        d = new_path.d()
+        path_el.set('d', d)
+
+        cx, cy = id_to_centroid[path_id]
+        cx, cy = cx + dx, cy + dy
 
         #3. Adjust text
         text_id = f"{path_id}n"
@@ -132,8 +187,8 @@ def scale_svg(input_svg: str,
         text_el.text = str(inner_text)
 
         text_scale = math.sqrt(id_to_norm_ev[path_id])
-        base_size = 20
-        new_size = base_size * text_scale
+        BASE_SIZE = 20
+        new_size = BASE_SIZE * text_scale
         digit_len = len(str(id_to_ev[path_id]))
         new_width = (new_size / 2) * digit_len
 
@@ -146,7 +201,7 @@ def scale_svg(input_svg: str,
             text_el.set('y', str(cy + new_size / 2.5))
             text_el.set('stroke-width', f'{0.5 * text_scale}')
         else:
-            text_el.set('font-size', str(base_size * 0.8))
+            text_el.set('font-size', str(BASE_SIZE * 0.8))
             text_el.set('stroke-width', f'{0.5}')
 
     # write out the modified SVG
